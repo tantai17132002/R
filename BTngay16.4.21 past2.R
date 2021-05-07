7db3d653c7b30659626abfb8b4500b49b329fa29
tantai17132002 <73747444+tantai17132002@users.noreply.github.com>
2021-05-07 11:28:24 +0700

    Add files via upload

---
+++ BTngay16.4.21 past2.R
@@ -0,0 +1,388 @@
+q()
+q()
+getwd()
+setwd("C:/Users/tanta/Documents")
+setwd("C:/Users/tanta/Downloads")
+getwd()
+setwd("~/")
+getwd()
+3+4
+4-1
+4*4
+5/3
+5^3
+x = 8^2/2
+x
+x <- 3^2-3+12
+x
+sqrt
+sqrt(4)
+abs(-31)
+sqrt4 = sqrt(4)
+sqrt(4)
+book1 = read.csv("Book1.csv")
+view(book1)
+View(book1)
+view(book1)
+book1 = read.csv("Book1.csv"
+)
+View(book1)
+str(book1)
+table(book1$Region)
+sumary(book1)
+summary(book1)
+max(book1$GNI)
+install.packages("xlsx")
+library("xlsx")
+library("xlsx")
+book2 = read.xlsx("Book1.xlsx")
+book2 = read.xlsx("Book1.xlsx")
+library("xlsx")
+install.packages("rJava")
+library("xlsx")
+install.packages("xlsx")
+library("xlsx")
+book = read.csv("Book1")
+book = read.csv("Book1.csv")
+View(book)
+library(rJava)
+library(xlsx)
+detach("package:xlsx", unload = TRUE)
+detach("package:rJava", unload = TRUE)
+library(rJava)
+detach("package:rJava", unload = TRUE)
+library(rJava)
+library(xlsx)
+library(xlsxjars)
+detach("package:xlsxjars", unload = TRUE)
+library(xlsxjars)
+library(class, lib.loc = "C:/Program Files/R/R-4.0.4/library")
+detach("package:class", unload = TRUE)
+detach("package:rJava", unload = TRUE)
+detach("package:xlsx", unload = TRUE)
+detach("package:xlsxjars", unload = TRUE)
+library(rJava)
+library(xlsx)
+detach("package:xlsx", unload = TRUE)
+detach("package:rJava", unload = TRUE)
+book = read.csv("Book1.csv")
+View(book)
+remove(Book1.csv)
+View(book)
+View(book)
+book = read.csv("book1.csv")
+View(book)
+plot(book1$)
+book = read.csv("Book.csv")
+View(book)
+plot(book$expenditure,book$eqv_income)
+plot(book$expenditure,book$eqv_income, xlab = "Expendicture", ylab = "Eqv_income", main= "So sánh Expendicture với Eqv_income")
+book = read.csv("Book.csv")
+> View(book)
+> plot(book$expenditure,book$eqv_income)
+> plot(book$expenditure,book$eqv_income, xlab = "Expendicture", ylab = "Eqv_income", main= "So sánh Expendicture và Eqv_income")
+>
+book = read.csv("Book.csv")
+> View(book)
+> plot(book$expenditure,book$eqv_income)
+> plot(book$expenditure,book$eqv_income, xlab = "Expendicture", ylab = "Eqv_income", main= "So sánh Expendicture và Eqv_income")
+>
+book = read.csv("Book.csv")> View(book)
+> plot(book$expenditure,book$eqv_income)
+> plot(book$expenditure,book$eqv_income, xlab = "Expendicture", ylab = "Eqv_income", main= "So sánh Expendicture và Eqv_income")
+>
+book = read.csv("Book.csv")
+plot(book$expenditure,book$eqv_income, xlab = "Expendicture", ylab = "Eqv_income", main= "So sánh Expendicture và Eqv_income")
+hist(book$tot_hhs)
+hist(book$tot_hhs, breaks = 50)
+hist(book$tot_hhs, breaks = 10)
+hist(book$tot_hhs, breaks = 20)
+hist(book$tot_hhs, breaks = 50)
+hist(book$tot_hhs, breaks = 30)
+hist(book$tot_hhs, breaks = 30, xlab = "Tot_hhs", ylab = "Frequecy", main = "Biểu đồ Histogram of Tot_hhs")
+hist(book$tot_hhs, breaks = 10, xlab = "Tot_hhs", ylab = "Frequecy", main = "Biểu đồ Histogram of Tot_hhs")
+hist(book$tot_hhs, breaks = 15, xlab = "Tot_hhs", ylab = "Frequecy", main = "Biểu đồ Histogram of Tot_hhs")
+hist(book$tot_hhs, breaks = 12, xlab = "Tot_hhs", ylab = "Frequecy", main = "Biểu đồ Histogram of Tot_hhs")
+hist(book$tot_hhs, breaks = 20, xlab = "Tot_hhs", ylab = "Frequecy", main = "Biểu đồ Histogram of Tot_hhs")
+hist(book$tot_hhs, breaks = 20, xlab = "Tot_hhs", ylab = "Frequecy", main = "Biểu đồ Histogram of Tot_hhs")
+hist(book$tot_hhs, breaks = 20, xlab = "Tot_hhs", ylab = "Frequecy", main = "Biểu đồ Histogram of Tot_hhs")
+boxplot(book$own)
+boxplot(book$income)
+boxplot(book$income ~ bool$eqv_exp)
+boxplot(book$income ~ book$eqv_exp)
+boxplot(book$income ~ book$expebditure)
+boxplot(book$income ~ book$expenditure)
+boxplot(book$income ~ book$age)
+boxplot(book$income ~ book$Region)
+boxplot(book$size ~ book$age)
+boxplot(book$eqv_income ~ book$age)
+boxplot(book$eqv_income ~ book$eqv_exp)
+boxplot(book$tot_hhs ~ book$own_wm)
+boxplot(book$eqv_income ~ book$age)
+boxplot(book$eqv_income ~ book$age, xlab = "Eqv_income", ylab = "Age", main = Biểu đồ Boxplot of Eqv_income and )
+boxplot(book$eqv_income ~ book$age, xlab = "Eqv_income", ylab = "Age", main = "Biểu đồ Boxplot of Eqv_income and Age")
+plot(book$income, book$eqv_exp , xlab = "Income", ylab = "Eqv_exp", main = "Biểu đồ")
++    xlab = "Income"
+plot(book$income, book$eqv_exp , xlab = "Income", ylab = "Eqv_exp", main = "Biểu đồ"
+d
+plot(book$income, book$eqv_exp, main = "Biểu đồ",
+xlab = "Income"
+ylab = "Eqv_exp"
+plot(book$income, book$eqv_exp, main = "Biểu đồ",
+xlab = "Income",
+ylab = "Eqv_exp")
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and ",
+r
+r
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = (0,100)
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = (0,100),
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = c(0,100),
+ylim = c(1000,10000),
+col = "gold",
+nch = 4)
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = c(0,100),
+ylim = c(1000,10000),
+col = "green",
+pch = 4)
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = c(1000,10000),
+ylim = c(10000,100000),
+col = "green",
+pch = 4)
+plot(book$income, book$eqv_exp , xlab = "Income", ylab = "Eqv_exp", main = "Biểu đồ")
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = c(10000,150000),
+ylim = c(10000,200000),
+col = "green",
+pch = 4)
+?points
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = c(10000,150000),
+ylim = c(10000,200000),
+col = "green",
+pch = 19)
+?points
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = c(10000,150000),
+ylim = c(10000,200000),
+col = "green",
+pch = 5)
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xlim = c(10000,150000),
+ylim = c(10000,200000),
+col = "green",
+pch = 2)
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+xtype = "l",
+ytype = "b",
+xlim = c(10000,150000),
+ylim = c(10000,200000),
+col = "green",
+pch = 2)
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+type = "l",
+xlim = c(10000,150000),
+ylim = c(10000,200000),
+col = "green",
+pch = 2)
+plot(book$income, book$eqv_exp, main = "Biểu đồ Scatterplot of Income and Eqv_exp",
+xlab = "Income",
+ylab = "Eqv_exp",
+type = "l",
+xlim = c(10000,150000),
+ylim = c(10000,200000),
+col = "green",
+pch = 2)
+?line
+book = read.csv("Book.csv")> View(book)
+View(book)
+book = read.csv("Book.csv")
+View(book)
+hist(book$own_prop, book$prop_hhs,
+abline(v=mean(book$own_prop))
+cs
+(book$own_prop, break = 10, freg = TRUE)
+hist(book$own_prop, breaks = 10, freg = TRUE)
+abline(v=mean(book$own_prop), lwd=4, col = "gold")
+abline(v=mean(book$own_prop), lwd=1, col = "gold")
+abline(v=mean(book$own_prop), lwd=1, col = "gold")
+abline(v=mean(book$own_prop), lwd=2, col = "gold")
+abline(v=mean(book$own_prop), lwd=3, col = "gold")
+abline(v=mean(book$own_prop), lwd=4, col = "gold")
+abline(v=mean(book$own_prop), lwd=3, col = "gold")
+abline(v=mean(book$own_prop), lwd=3, col = "red")
+abline(v=mean(book$own_prop), lwd=3, col = "red")
+abline(v=mean(book$own_prop), lwd=2, col = "red")
+hist(book$own_prop, breaks = 10, freq = FALSE)
+hist(book$own_prop, breaks = 10, freq = FALSE)
+abline(v=mean(book$own_prop), lwd=2, col = "red")
+abline(v=mean(book$own_prop), lwd=3, col = "red")
+abline(v=mean(book$own_prop), lwd=3, col = "red")
+hist(book$own_prop, breaks = 10, freq = FALSE, main = "BIỂU ĐỒ HISTOGRAM OF OWN_PROP",
+xlab = Own_prop,
+ylab = density,
+abline(v=mean(book$own_prop), lwd=3, col = "red"))
+hist(book$own_prop, breaks = 10, freq = FALSE, main = "BIỂU ĐỒ HISTOGRAM OF OWN_PROP",
+xlab = Own_prop,
+ylab = density)
+hist(book$own_prop, breaks = 10, freq = FALSE, main = "BIỂU ĐỒ HISTOGRAM OF OWN_PROP",
+xlab = Own_prop,
+ylab = density)
+hist(book$own_prop, breaks = 10, freq = FALSE, main = "BIỂU ĐỒ HISTOGRAM OF OWN_PROP",
+xlab = Own_prop)
+abline(v=mean(book$own_prop), lwd=3, col = "red")
+hist(book$own_prop, breaks = 10, freq = FALSE, main = "BIỂU ĐỒ HISTOGRAM OF OWN_PROP")
+abline(v=mean(book$own_prop), lwd=3, col = "red")
+if (x<10) {x = x-5}
+if (x<10) {x = 5-x}
+if (x>5) {x = x-5}
+if (x>5) {x = x-5}
+x = 4
+if (x>5) {x = x-5}
+x
+if (x>1) {x = x-5}
+x
+x
+if (x>2) {x = x-5}
+x
+if (x>3) {x = x-3}
+x
+x = 4
+if (x>3) {x = x-3}
+x
+x = 4
+if (x>4) {x = x-4} else {x = 3-x}
+x
+if (x>4) {x = x-4} else if (x==4){ x = 0} else {x = 3-x}
+x
+if(y[i] >0) {a = a-1} }
+}
+y = c(3,4,1,-2,9,0,-1,5)
+a = 0
+for (i in 1:legth(y){
+for (i in 1:length(y){
+for (i in 1:length(y){
+y = c(3,4,1,-2,9,0,-1,5)
+a = 0
+for (i in 1:length(y){
+if(y[i] >0) {a = a+1}
+}
+for (i in 1:length(y)){
+if(y[i] >0) {a = a+1}
+}
+a
+if(y[i] >0) {a = a+1}}
+if(y[i] >0) {a = a+1} }
+y = c(3,4,1,-2,9,0,-1,5)
+a = 0
+for (i in 1:length(y)){
+if(y[i] >0) {a = a+1} }
+y = c(3,4,1,-2,9,0,-1,5)
+a = 0
+for (i in 1:length(y)){
+if(y[i] >0) {a = a+1}}
+a
+sum(y>0)
+y>0
+a = function(vect){
+sum(is.a(vect))
+}
+a(c(3,4,1,a,4,a))
+a = function(vect){
+sum(is.a(vect))
+}
+a(c(3,4,1,X,4,X))
+a = function(vect){
+sum(is.a(vect))
+}
+c
+sum(is.na(vect))
+a = function(vect){
+sum(is.na(vect))
+}
+a(c(3,4,1,X,4,X))
+a = function(vect){
+sum(is.na(vect))
+}
+a(c(3,4,1,NA,4,NA))
+matran = matrix(c(1,3,4,2,6),7,8)
+matran = matrix(c(1,3,4,2,6),2,3)
+matran = matrix(c(1,3,4,2,6),2,3)
+matran = matrix(c(1,3,4,2,6),3,2)
+matran = matrix(c(1,3,4,2,5,6),3,2)
+matran
+apply(matran,2,a)
+apply(matran,1,sum)
+apply(matran,2,sum)
+list1 = list(c(1,4,5),5,2,c(4,3,5))
+lappy(list1.sum)
+list1 = list(c(1,4,5),5,2,c(4,3,5))
+lappyy(list1.sum)
+list1 = list(c(1,4,5),5,2,c(4,3,5))
+lapply(list1.sum)
+list1 = list(c(1,9,5),4,2,c(2,3,6))
+list1.sum
+y = c(3,4,1,-2,9,0,-1,5)
+a = 0
+for (i in 1:length(y)){
+if(y[i] >0) {a = a+1}}
+list1 = list(c(1,9,5),4,2,c(2,3,6))
+lapply(list1,sum)
+list1 = list(c(1,9,5),4,2,c(2,3,6))
+lapply(list1,length)
+list1 = list(c(4,5,6),3,2,c(7,8,9))
+sapply(list1,length)
+list1 = list(c(4,5,6),3,2,c(7,8,9))
+sapply(list1,length)
+book = read("book.csv")
+book = read.csv("book.csv")
+View(book)
+tapply(book$size,book$age,mean)
+tapply(book$size,book$age,sum)
+setwd("E:\\Document\\R\\.R")
+a = read.csv("Temperature.csv", header = TRUE)
+View(a)
+trung_binh = tapply(a$Temperature, a$Month, mean, na.rm = TRUE)
+trung_binh
+trung_binh2 = tapply(a$Temperature, list(a$Year, a$Month), mean, na.rm = TRUE)
+trinh_binh2
+trung_binh2 = tapply(a$Temperature, list(a$Year, a$Month), mean, na.rm = TRUE)
+trung_binh2
+trung_binh3 = tapply(a$Temperature, a$Month, sd, na.rm = TRUE)
+trung_binh3
+View(trung_binh2)
+View(trung_binh2)
+View(trung_binh2)
+View(a)
+View(trung_binh2)
+trung_binh3 = tapply(a$Temperature, a$Month, sd, na.rm = TRUE)
+trung_binh3
+help(table)
+table(a$Year)
+table(a$Station, a$Year)
